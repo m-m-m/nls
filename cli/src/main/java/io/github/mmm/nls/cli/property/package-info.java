7729e2b5a06f7b5aeea7215d0a8035d8182c0197
Jörg Hohwiller <joerg.hohwiller@capgemini.com>
2023-02-18 23:46:03 +0100

    moved cli stuff to cli repo

--- cli/src/main/java/io/github/mmm/nls/cli/property/package-info.java
+++
@@ -1,6 +0,0 @@
-/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
- * http://www.apache.org/licenses/LICENSE-2.0 */
-/**
- * Contains properties for data-binding to CLI commands.
- */
-package io.github.mmm.nls.cli.property;
--- sync/src/main/java/io/github/mmm/nls/sync/package-info.java
+++
@@ -1,6 +0,0 @@
-/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
- * http://www.apache.org/licenses/LICENSE-2.0 */
-/**
- * Provides a command-line-interface to synchronize resource bundles for localization.
- */
-package io.github.mmm.nls.sync;
